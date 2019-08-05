4bd8f3e091d50bce2c7e32ec1a8f3ce50bebc3e0
thegamlion <thegamlion@gmail.com>
2019-08-05 02:34:57 +0200

    newe stuff

--- test2.py
+++ test2.py
@@ -1,10 +1,17 @@
 import bs4 as bs
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
 import datetime as dt
 import os
+import pandas as pd
 import pandas_datareader.data as web
 import pickle
 import requests
 import yfinance as yf
+
+style.use('ggplot')
+
 
 def save_sp500_tickers():
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -19,7 +26,6 @@ def save_sp500_tickers():
     tickers = [t.replace('\n', '') for t in tickers]
     return tickers
 
-
 # save_sp500_tickers()
 
 def get_data_from_yahoo(reload_sp500=False):
@@ -31,7 +37,7 @@ def get_data_from_yahoo(reload_sp500=False):
     if not os.path.exists('stock_dfs'):
         os.makedirs('stock_dfs')
 
-    start = dt.datetime(2010, 1, 1)
+    start = dt.datetime(2000, 1, 1)
     end = dt.datetime.now()
     for ticker in tickers:
         # just in case your connection breaks, we'd like to save our progress!
@@ -42,4 +48,55 @@ def get_data_from_yahoo(reload_sp500=False):
             print('Already have {}'.format(ticker))
 
 
-get_data_from_yahoo()
+#get_data_from_yahoo()
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close': ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+
+#compile_data()
+def visualize_data():
+    df = pd.read_csv('sp500_joined_closes.csv')
+    df_corr = df.corr()
+    df_corr.to_csv('sp500corr.csv')
+    data1 = df_corr.values
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111)
+
+    heatmap1 = ax1.pcolor(data1, cmap=plt.cm.RdYlGn)
+    fig1.colorbar(heatmap1)
+
+    ax1.set_xticks(np.arange(data1.shape[1]) + 0.5, minor=False)
+    ax1.set_yticks(np.arange(data1.shape[0]) + 0.5, minor=False)
+    ax1.invert_yaxis()
+    ax1.xaxis.tick_top()
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    ax1.set_xticklabels(column_labels)
+    ax1.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap1.set_clim(-1, 1)
+    plt.tight_layout()
+    plt.show()
+
+#visualize_data()
+
